1d1fe48dac455cbffde093437b2e3e5d0a6c6336
Evgeny Kuzin <evgeny.kuzin@outlook.com>
2022-05-22 20:42:28 +0100

    init

--- storage/storage.go
+++ storage/storage.go
@@ -6,6 +6,9 @@ import (
 	"github.com/evkuzin/weatherstation/weather_station"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
 	"time"
 )
 
@@ -21,13 +24,22 @@ type Storage struct {
 }
 
 func (s *Storage) Init(config *config.Config) error {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
+		},
+	)
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		config.Database.Host,
 		config.Database.User,
 		config.Database.Password,
 		config.Database.Database,
 		config.Database.Port)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		return err
 	}
